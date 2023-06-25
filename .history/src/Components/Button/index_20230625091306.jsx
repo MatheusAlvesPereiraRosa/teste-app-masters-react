227d35c22e9c507649c22877142874692b8bd034
Matheus Alves <matpr.a98@gmail.com>
2023-06-25 09:15:29 -0300

    Corrigindo mais algumas coisas

--- .history/src/Components/Button/index_20230625091306.jsx
+++ .history/src/Components/Button/index_20230625091306.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 export const Button = ({ onClick }) => (
   <button onClick={onClick} className="btn-load">
-    <div className="btn-text" type="button">
+    <div className="btn-text" typeof="button">
       Load more...
     </div>
   </button>
